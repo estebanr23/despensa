86cea0760dc6ea90e0d1ba5d56fd0252fc41fc5d
Esteban Robert <esteban.robert23@gmail.com>
2022-09-14 12:02:44 -0300

    Modificando carrito de ventas

--- resources/views/sales/create.blade.php
+++ resources/views/sales/create.blade.php
@@ -27,7 +27,7 @@
                                 <label>Producto</label>
                                 <select name="product_id" id="product_id" class="form-control select2" style="width: 100%;">
                                     @foreach ($products as $product)
-                                        <option value="{{ $product->id }}">{{ $product->nombre_prod }}</option>
+                                        <option value="{{ $product->id }}" data-precio="{{ $product->precio_prod }}">{{ $product->nombre_prod }}</option>
                                     @endforeach
                                 </select>
                             </div>
@@ -50,16 +50,65 @@
                 
                 </div>
                 <!-- /.card-body -->
-                <div class="card-footer">Visit for more examples and information about the plugin.</div>
+                <div class="card-footer">Lista de productos seleccionados</div>
 
 
-                <div class="card-body" id="items">
-                    {{-- <div class="col-md-3">
-                        <span></span>
-                    </div> --}}
-                </div>
+                {{-- <div class="card-body" id="items">
+
+                </div> --}}
                 <!-- /.row -->
 
+
+
+
+                <div class="invoice p-3 mb-3">
+                    <!-- Table row -->
+                    <div class="row">
+                      <div class="col-12 table-responsive">
+                        <table class="table table-striped">
+                          <thead>
+                          <tr>
+                            <th>Producto</th>
+                            <th>Cantidad</th>
+                            <th>Precio</th>
+                            <th>Subtotal</th>
+                            <th>Acciones</th>
+                          </tr>
+                          </thead>
+                          <tbody id="items"></tbody>
+                        </table>
+                      </div>
+                      <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+      
+                    <div class="row">
+                      <div class="col-6">
+                        <p class="lead">Amount Due 2/22/2014</p>
+      
+                        <div class="table-responsive">
+                            <table class="table">
+                                <tbody>
+                                <tr>
+                                    <th style="width:50%">Total:</th>
+                                    <td id="total"></td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                      </div>
+                      <!-- /.col -->
+                    </div>
+                    <!-- /.row -->
+      
+                    <!-- this row will not appear when printing -->
+                    <div class="row no-print">
+                      <div class="col-12">
+                        <button type="button" class="btn btn-danger float-right"><i class="fas fa-trash"></i> Cancelar</button>
+                        <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;"><i class="far fa-credit-card"></i> Finalizar</button>
+                      </div>
+                    </div>
+                </div>
 
             </div>
         </div>
@@ -73,16 +122,21 @@
 
             $("button").click(function() {
                 const product = $('#product_id  option:selected');
-                const cant_prod = $('#cant_order_prod').val();      
+                const cant_prod = $('#cant_order_prod').val();   
                 crearItem(product, cant_prod);
+                calcularTotal();
             });  
             
             function crearItem(product, cant_prod) {
+                const precio = product.attr('data-precio')
                 const item = {
                     id: product.val(),
                     nombre: product.text(),
                     cantidad: cant_prod,
+                    precio: precio,
+                    subTotal: (cant_prod * precio).toFixed(2),
                 }
+                
 
                 const busqueda = carrito.find((element) => element.id === item.id);
                 if(!busqueda) {
@@ -93,22 +147,29 @@
             }
 
             function crearElement(item) {
-                let div = $("<div class='row'></div>");
+                let fila = $("<tr></tr>");
                 let elemento = '';
-                elemento += "<div class='col-md-3'><p>"+item.id+"</p></div>";
-                elemento += "<div class='col-md-3'><p>"+item.nombre+"</p></div>";
-                elemento += "<div class='col-md-3'><p>"+item.cantidad+"</p></div>";
-                elemento += "<div class='col-md-3'><buttom class='btn btn-danger'>Eliminar</buttom></div>";
+                elemento += "<td><p>"+item.nombre+"</p></td>";
+                elemento += "<td><p>"+item.cantidad+"</p></td>";
+                elemento += "<td><p>$ "+item.precio+"</p></td>";
+                elemento += "<td><p>$ "+item.subTotal+"</p></td>";
+                elemento += "<td><button id='"+item.id+"' class='btn hola btn-danger'>Eliminar</button></td>";
+
                 
-                $(div).append(elemento);
-                $('#items').append(div);
+                $(fila).append(elemento);
+                $('#items').append(fila);
             }
 
+            function calcularTotal() {
+                let total = carrito.reduce((acumulador, item) => acumulador + Number(item.subTotal), 0);
+                $('#total').text(`$ ${total}`);
+            }
 
         });
+
+
     </script>
 @endpush
-
 
 
 
