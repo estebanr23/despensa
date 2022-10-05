9a1e6eba3a88bd7c5b6d41a11d18808188502560
Esteban Robert <esteban.robert23@gmail.com>
2022-10-05 09:38:15 -0300

    Modificacion de la plantilla

--- resources/views/layout/plantilla.blade.php
+++ resources/views/layout/plantilla.blade.php
@@ -268,13 +268,13 @@
               </a>
               <ul class="nav nav-treeview">
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="{{ Route('orders.create') }}" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Agregar Pedido</p>
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="#" class="nav-link">
+                  <a href="{{ Route('orders.index') }}" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Lista de Pedidos</p>
                   </a>
